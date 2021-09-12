1d3057b16c653aa822fce8a8dc9336749d90f3ad
Ethan Zemelman <ethanzemelman@gmail.com>
2021-09-12 19:21:49 -0400

    Finish Forecast model
    Also changed the snake case to camel case.

--- Shared/Model/Forecast.swift
+++ Shared/Model/Forecast.swift
@@ -13,25 +13,35 @@ struct Forecast: Codable {
         let sunrise: Date
         let sunset: Date
         let temp: Double
-        let feels_like: Double
+        let feelsLike: Double
         struct Weather: Codable {
             let description: String
             let icon: String
+            var weatherIconURL: URL? {
+                let urlString = "https://openweathermap.org/img/wn/\(icon)@2x.png"
+                return URL(string: urlString)
+            }
         }
         let weather: [Weather]
     }
     let current: Current
+    
     struct Hourly: Codable {
         let dt: Date
         let temp: Double
-        let feels_like: Double
+        let feelsLike: Double
         struct Weather: Codable {
             let description: String
             let icon: String
+            var weatherIconURL: URL? {
+                let urlString = "https://openweathermap.org/img/wn/\(icon)@2x.png"
+                return URL(string: urlString)
+            }
         }
         let weather: [Weather]
     }
     let hourly: [Hourly]
+    
     struct Daily: Codable {
         let dt: Date
         let sunrise: Date
@@ -41,5 +51,15 @@ struct Forecast: Codable {
             let max: Double
         }
         let temp: Temp
+        struct Weather: Codable {
+            let description: String
+            let icon: String
+            var weatherIconURL: URL? {
+                let urlString = "https://openweathermap.org/img/wn/\(icon)@2x.png"
+                return URL(string: urlString)
+            }
+        }
+        let weather: [Weather]
     }
+    let daily: [Daily]
 }
