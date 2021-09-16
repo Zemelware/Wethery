bce135bf64e5c8734bb90810ceefaa9d0d2a910c
Ethan Zemelman <ethanzemelman@gmail.com>
2021-09-16 17:47:56 -0400

    Create MockData and extract all structs within the Forecast struct

--- Shared/Models/Forecast.swift
+++ Shared/Models/Forecast.swift
@@ -8,58 +8,52 @@
 import Foundation
 
 struct Forecast: Codable {
-    struct Current: Codable {
-        let dt: Date
-        let sunrise: Date
-        let sunset: Date
-        let temp: Double
-        let feelsLike: Double
-        struct Weather: Codable {
-            let description: String
-            let icon: String
-            var weatherIconURL: URL? {
-                let urlString = "https://openweathermap.org/img/wn/\(icon)@2x.png"
-                return URL(string: urlString)
-            }
-        }
-        let weather: [Weather]
-    }
     let current: Current
-    
-    struct Hourly: Codable {
-        let dt: Date
-        let temp: Double
-        let feelsLike: Double
-        struct Weather: Codable {
-            let description: String
-            let icon: String
-            var weatherIconURL: URL? {
-                let urlString = "https://openweathermap.org/img/wn/\(icon)@2x.png"
-                return URL(string: urlString)
-            }
-        }
-        let weather: [Weather]
-    }
     let hourly: [Hourly]
-    
-    struct Daily: Codable {
-        let dt: Date
-        let sunrise: Date
-        let sunset: Date
-        struct Temp: Codable {
-            let min: Double
-            let max: Double
-        }
-        let temp: Temp
-        struct Weather: Codable {
-            let description: String
-            let icon: String
-            var weatherIconURL: URL? {
-                let urlString = "https://openweathermap.org/img/wn/\(icon)@2x.png"
-                return URL(string: urlString)
-            }
-        }
-        let weather: [Weather]
-    }
     let daily: [Daily]
+}
+
+struct Current: Codable {
+    let dt: Date
+    let sunrise: Date
+    let sunset: Date
+    let temp: Double
+    let feelsLike: Double
+    let weather: [Weather]
+}
+
+struct Hourly: Codable {
+    let dt: Date
+    let temp: Double
+    let feelsLike: Double
+    let weather: [Weather]
+}
+
+struct Daily: Codable {
+    let dt: Date
+    let sunrise: Date
+    let sunset: Date
+    let temp: Temp
+    let weather: [Weather]
+}
+
+struct Temp: Codable {
+    let min: Double
+    let max: Double
+}
+
+struct Weather: Codable {
+    let description: String
+    let icon: String
+    var weatherIconURL: URL? {
+        let urlString = "https://openweathermap.org/img/wn/\(icon)@2x.png"
+        return URL(string: urlString)
+    }
+}
+
+
+struct MockData {
+    static let forecast = Forecast(current: current, hourly: [], daily: [])
+    static let current = Current(dt: Date(timeIntervalSince1970: 1631827700), sunrise: Date(timeIntervalSince1970: 1631838232), sunset: Date(timeIntervalSince1970: 1631883022), temp: 21.3, feelsLike: 23.1, weather: weather)
+    static let weather = [Weather(description: "Broken Clouds", icon: "04n")]
 }
